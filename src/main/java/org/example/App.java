7f2760fc5975933a892a850d4f4e98bd8ef726c7
William Mallett <mallett002@gmail.com>
2023-04-08 08:41:49 -0500

    sort confrences by average points per game

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,9 +12,43 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class App {
+    ObjectMapper mapper;
+
+    public App() {
+        mapper = new ObjectMapper();
+    }
 
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public List<String> sortConferencesByAveragePointsPerGame() throws IOException {
+        InputStream conferenceStream = Conference.class.getResourceAsStream("/conferences.json");
+        InputStream teamStream = Conference.class.getResourceAsStream("/teams.json");
+
+        List<Conference> conferences = mapper.readValue(conferenceStream, new TypeReference<List<Conference>>(){});
+        List<Team> teams = mapper.readValue(teamStream, new TypeReference<List<Team>>(){});
+
+        // Create new comparator to sort based on ppg:
+        Comparator<Conference> ppgComparator = (c1, c2) -> {
+            Double c1PPG = c1.getTeams().stream()
+                .map(teamId -> teams.stream().filter(t -> t.getId().equals(teamId)).findFirst().get())
+                .mapToDouble(t -> t.getPointsPerGame())
+                .sum();
+
+            Double c2PPG = c2.getTeams().stream()
+                    .map(teamId -> teams.stream().filter(t -> t.getId().equals(teamId)).findFirst().get())
+                    .mapToDouble(t -> t.getPointsPerGame())
+                    .sum();
+
+            return c2PPG.compareTo(c1PPG);
+        };
+
+        // sort the list of conferences by the summed total ppg of the teams in that conference
+        return conferences.stream()
+                .sorted(ppgComparator)
+                .map(Conference::getName)
+                .collect(Collectors.toList());
     }
 }
