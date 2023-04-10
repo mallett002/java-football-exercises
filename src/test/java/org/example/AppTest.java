3bc6c6795ad7f5f3f28b92c012d8791d88abd578
William Mallett <mallett002@gmail.com>
2023-04-10 06:16:53 -0500

    update test name

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -34,7 +34,7 @@ public class AppTest {
     }
 
     @Test
-    public void testGetTeamsWithMostPointsThroughUprights() {
+    public void testGetTeamOrTeamsWithMostPointsThroughUprights() {
 
     }
 
