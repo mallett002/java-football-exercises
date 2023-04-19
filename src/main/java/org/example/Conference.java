da399a36a331bc5bdf9c6d95a71dcf7b61f165ac
William Mallett <mallett002@gmail.com>
2023-04-19 05:54:23 -0500

    more cleanup

--- src/main/java/org/example/Conference.java
+++ src/main/java/org/example/Conference.java
@@ -1,10 +1,7 @@
 package org.example;
 
-import lombok.Data;
-
 import java.util.List;
 
-@Data
 public class Conference {
     private String id;
     private String name;
--- src/main/java/org/example/Team.java
+++ src/main/java/org/example/Team.java
@@ -1,12 +1,6 @@
 package org.example;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Data;
 
-@Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class Team {
     private Integer fieldGoals;
     private Integer games;
